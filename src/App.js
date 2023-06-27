80caba5d39378e129e90585fb44ecf3afa634726
hugop <hugopinho07@gmail.com>
2023-06-27 20:16:03 -0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Products from "./pages/Products";
 
 function App() {
   return (
@@ -8,7 +11,9 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/reports" element={<Reports />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </Router>
     </>
